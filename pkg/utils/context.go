16fd5391e2751fa52b8d02ce119b1d561d0a8125
Nils Gustav Stråbø <nst@equinor.com>
2022-04-04 09:52:12 +0200

    create server

---
+++ pkg/utils/context.go
@@ -0,0 +1,16 @@
+package utils
+
+import "context"
+
+func ContextForChannel(parentCh <-chan struct{}) (context.Context, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		select {
+		case <-parentCh:
+			cancel()
+		case <-ctx.Done():
+		}
+	}()
+	return ctx, cancel
+}
