11be478035c701bfaeb87487514e2084fd84d90a
Richard Hagen <richard.hagen@bouvet.no>
2024-02-06 15:46:03 +0100

    simplify workflows, use new managed identity for database interactions

--- .github/workflows/scripts/blacklist-ip.ps1
+++
@@ -1,8 +0,0 @@
-param (
-    [Parameter(Mandatory=$true)][string]$FirewallRuleName
- )
-
-az sql server firewall-rule delete `
-    --name ${FirewallRuleName} `
-    --resource-group ${env:RESOURCE_GROUP} `
-    --server ${env:SQL_SERVER_NAME}
